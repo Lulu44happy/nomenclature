3f07ebbadba7c31d441ca8a74d8e09423721ee06
Lulu44happy <133497676+Lulu44happy@users.noreply.github.com>
2025-04-05 15:50:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, send_file
 import os
-
+from generate import generate_image
 app = Flask(__name__)
 
 @app.route("/")
-def generate():
-    # Appel de la fonction de génération de l'image (qui doit être définie ailleurs)
-    from generate import generate_image
-    generate_image()  # Génère l'image et la sauve dans le dossier static
-    return "Image générée avec succès!"
 def index():
+    generate_image()  # Génère l'image et la sauve dans le dossier static
     return render_template("index.html")  # Assurez-vous que index.html est dans le dossier templates
 
 
